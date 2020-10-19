92df48161cab14aac54d0d7b714b7f07b529beba
Daniel Volquardsen <daniel@volquardsen.dev>
2020-10-19 09:46:16 +0200

    dev

--- src/transform/DocumentFile.ts
+++ src/transform/DocumentFile.ts
@@ -5,14 +5,28 @@ import { transformFragmentDefinitionNode } from './Fragment'
 import { OperationDefinitionNode, FragmentDefinitionNode } from 'graphql'
 
 export function transformDocumentFile(file: Types.DocumentFile): FileIR {
+      //@ts-ignore
+      if (file.content){
+         //@ts-ignore
+        file.document = file.content;
+    }
+  
   return {
+    //@ts-ignore
     filePath: file.filePath,
-    operations: file.content.definitions
+    //@ts-ignore
+    operations: file.document.definitions
+      //@ts-ignore
       .filter(it => it.kind == 'OperationDefinition')
+      //@ts-ignore
       .map(node => node as OperationDefinitionNode)
       .map(transformOperationDefinitionNode),
-    fragments: file.content.definitions
+    //@ts-ignore
+
+    fragments: file.document.definitions
+      //@ts-ignore
       .filter(it => it.kind == 'FragmentDefinition')
+      //@ts-ignore
       .map(node => node as FragmentDefinitionNode)
       .map(transformFragmentDefinitionNode),
   }
